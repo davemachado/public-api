ba9e0569cdc85fea1b3709d34418e9bc982b7888
Dave Machado <dave@davemachado.com>
2018-01-17 19:37:43 -0500

    Add Test Cases for intial endpoints

--- handlers_test.go
+++ handlers_test.go
@@ -6,6 +6,29 @@ import (
 	"testing"
 )
 
+func TestCheckEntryMatches(t *testing.T) {
+	entry := Entry{
+		API:         "examplesAsAService",
+		Description: "provide classic examples of classic things",
+		Auth:        "apiKey",
+		HTTPS:       true,
+		Link:        "http://www.example.com",
+		Category:    "Development",
+	}
+	search := &SearchRequest{}
+	if !checkEntryMatches(entry, search) {
+		t.Errorf("failed to match entry and search")
+	}
+	search.HTTPS = "true"
+	if !checkEntryMatches(entry, search) {
+		t.Errorf("failed to match entry and search")
+	}
+	search.Auth = "OAuth"
+	if checkEntryMatches(entry, search) {
+		t.Errorf("failed to match entry and search")
+	}
+}
+
 func TestHealthCheckHandler(t *testing.T) {
 	req, err := http.NewRequest("GET", "/health-check", nil)
 	if err != nil {
@@ -40,5 +63,20 @@ func TestGetEntriesHandler(t *testing.T) {
 	if status := rr.Code; status != http.StatusOK {
 		t.Errorf("handler returned wrong status code: got %v want %v",
 			status, http.StatusOK)
+	}
+}
+func TestGetEntriesWithBadMethod(t *testing.T) {
+	req, err := http.NewRequest("POST", "/api", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(getEntriesHandler)
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusMethodNotAllowed {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusMethodNotAllowed)
 	}
 }
